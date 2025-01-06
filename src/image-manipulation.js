fb70732e220ab9d48828ce9344e31f352e0edb96
Isaiah Odhner <isaiahodhner@gmail.com>
2025-01-06 02:32:17 -0500

    Simplify variable declarations

--- src/image-manipulation.js
+++ src/image-manipulation.js
@@ -1103,12 +1103,11 @@ function compute_bezier(t, start_x, start_y, control_1_x, control_1_y, control_2
 	const mt = 1 - t;
 	const mt2 = mt * mt;
 	const t2 = t * t;
-	let a = 0, b = 0, c = 0, d = 0;
 
-	a = mt2 * mt;
-	b = mt2 * t * 3;
-	c = mt * t2 * 3;
-	d = t * t2;
+	const a = mt2 * mt;
+	const b = mt2 * t * 3;
+	const c = mt * t2 * 3;
+	const d = t * t2;
 
 	return {
 		x: a * start_x + b * control_1_x + c * control_2_x + d * end_x,
